741c0852dfc1d621d7a2ade5888e5f1034a3ee1f
mills892003 <38229907+mills892003@users.noreply.github.com>
2019-08-07 02:46:10 -0400

    Add files via upload
    Extended version of the c++ program

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//  Created by Milan Patel on 9/4/18.
+//  Created by Milan Patel on 10/2/18.
 //  Copyright © 2018 Milan Patel. All rights reserved.
 //
 
@@ -6,6 +6,8 @@
 #include <iomanip>
 #include <cmath>
 #include <fstream>
+
+float Display(float, int);
 
 using namespace std;
 
@@ -15,22 +17,46 @@ int main()
     ofstream outputfile;
     
     float score, total, fscore;     //Declare test variables
+    int rscore;
     
     inputfile.open("score.txt");        //open input file
     outputfile.open("final score.txt"); //open output file
     inputfile >> score >> total;        //read input file and store variables
-    fscore = (score / total)*100;             //calculate final score
-    
+    fscore = (score / total)*100;       //calculate final score
+    rscore = ceil(fscore);              //round final score
     
     //Write results in output file
-    outputfile << fixed << "Final score is " << setprecision(0) << ceil(fscore) << "%" << endl;
+    outputfile << fixed << "Final score is " << setprecision(0) << rscore << "%" << endl;
     outputfile << fixed << "Final score is " << setprecision(5) << fscore << "%" << endl;
-
-    //Display results
-    cout << fixed << "Final score is " << setprecision(0) << ceil(fscore) << "%" << endl;
-    cout << fixed << "Final score is " << setprecision(5) << fscore << "%" << endl;
+    
+    // Display results function
+    Display ( fscore, rscore);
     
     getchar();          //hold curser for user
     inputfile.close();  //close input file
     return 0;
+}
+
+float Display(float fscore, int rscore)       //Display function
+{
+    //Display results
+    cout << fixed << "Final score is " << setprecision(0) << rscore << "%" << endl;
+    cout << fixed << "Final score is " << setprecision(5) << fscore << "%" << endl;
+    
+    if (fscore > 90) {
+        cout << "Excellent" << endl;
+    }
+    else if (fscore > 80) {
+        cout << "Well Done" << endl;
+    }
+    else if (fscore > 70) {
+        cout << "Good" << endl;
+    }
+    else if (fscore >= 60) {
+        cout << "Need Improvement" << endl;
+    }
+    else if (fscore <= 50) {
+        cout << "Fail" << endl;
+    }
+    return (0);
 }
